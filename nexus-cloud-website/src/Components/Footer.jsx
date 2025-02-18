0d892749a03dfb72add4122e1cd2060b3466a126
JesnerPerillo <jesnerperillo15@gmail.com>
2025-02-18 14:54:09 +0800

    Added link to socials

--- nexus-cloud-website/src/Components/Footer.jsx
+++ nexus-cloud-website/src/Components/Footer.jsx
@@ -35,10 +35,10 @@ export default function Footer() {
                 {/*Bottom Footer */}
                 <div className="w-4/5 h-32 flex flex-col items-center justify-around">
                     <div className="flex justify-center gap-5 items-center ">
-                        <LiaFacebook className="w-8 h-8"/>
-                        <TfiInstagram className="w-6 h-6"/>
-                        <TiSocialYoutubeCircular  className="w-8 h-8"/>
-                        <FaTiktok className="w-5 h-5"/>
+                        <a href="https://web.facebook.com/nxs88"><LiaFacebook className="w-8 h-8"/></a>
+                        <a href="https://www.instagram.com/nexuscloud_/?next=%2F"><TfiInstagram className="w-6 h-6"/></a>
+                        <a href="https://www.youtube.com/@NexuscloudITSolutions"><TiSocialYoutubeCircular  className="w-8 h-8"/></a>
+                        <a href="https://www.tiktok.com/@nexuscloud_"><FaTiktok className="w-5 h-5"/></a>
                     </div>
                     <div>
                         <p>&copy; Copyrights. All rights reserved</p>
