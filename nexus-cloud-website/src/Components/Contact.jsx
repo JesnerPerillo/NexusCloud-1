7ab01495c29553411758d6418dd8f5e9eea53e81
Jesner Perillo <perillojesner15@gmail.com>
2025-03-24 11:22:41 +0800

    Modified Contact

--- nexus-cloud-website/src/Components/Contact.jsx
+++ nexus-cloud-website/src/Components/Contact.jsx
@@ -8,28 +8,28 @@ export default function Contact() {
     <div className="con bg-gray-100 w-full h-auto sm:w-full h-auto py-16 px-6">
       <div className="max-w-screen-xl mx-auto flex flex-col gap-16 sm:flex-row sm:gap-32 items-center justify-center ">
         {/* Contact Section */}
-        <div className="bg-white relative rounded-xl shadow-xl p-8 flex flex-col items-start justify-between w-full sm:w-1/2 space-y-6 transition-transform transform hover:scale-105 hover:shadow-2xl overflow-hidden">
-          <div className="absolute w-180 h-180 bg-blue-600 rounded-full opacity-50 top-[-3rem] right-20 z-10"></div>
-          <h1 className="text-3xl font-semibold text-gray-800 mb-4 z-20">CONTACT US</h1>
+        <div className="con relative rounded-xl shadow-xl p-8 flex flex-col items-start justify-between w-full sm:w-1/2 space-y-6 transition-transform transform hover:scale-105 hover:shadow-2xl overflow-hidden">
+          <div className="absolute w-180 h-180 footer rounded-full opacity-50 top-[-3rem] right-20 z-10"></div>
+          <h1 className="text-3xl font-semibold mb-4 z-20">CONTACT US</h1>
           <div className="space-y-4 z-20">
             {/* Address */}
-            <div className="flex items-center text-lg text-gray-700 space-x-4 transition-all hover:text-[#f4ca4f]">
+            <div className="flex items-center text-lg space-x-4 transition-all hover:text-[#f4ca4f]">
               <a href="https://www.google.com/maps/place/NEXUSCLOUD+IT+SOLUTIONS+INC/@14.5896052,121.0584803,17z/data=!3m1!4b1!4m6!3m5!1s0x3397c8170cbf03d9:0xcfe9a8f04e9cc91!8m2!3d14.5896052!4d121.0610552!16s%2Fg%2F11smm1k9bz?entry=ttu&g_ep=EgoyMDI1MDIxNy4wIKXMDSoASAFQAw%3D%3D">
-                <PiMapPinLight className="h-8 w-8 text-black" />
+                <PiMapPinLight className="h-8 w-8" />
               </a>
               <p className="hover:cursor-pointer">U1414 14TH FL. AIC BURGUNDY EMPIRE TOWER II, ADB AVE. ORTIGAS CENTER PASIG CITY</p>
             </div>
 
             {/* Phone Number */}
-            <div className="flex items-center text-lg text-gray-700 space-x-4 transition-all hover:text-[#f4ca4f]">
-              <BsTelephone className="w-8 h-8 text-black" />
+            <div className="flex items-center text-lg space-x-4 transition-all hover:text-[#f4ca4f]">
+              <BsTelephone className="w-8 h-8" />
               <p className="hover:cursor-pointer">+63 995 8494 428</p>
             </div>
 
             {/* Email */}
-            <div className="flex items-center text-lg text-gray-700 space-x-4 transition-all hover:text-[#f4ca4f]">
+            <div className="flex items-center text-lg space-x-4 transition-all hover:text-[#f4ca4f]">
               <a href="mailto:kdoz@live.com">
-                <CiMail className="h-8 w-8 text-black" />
+                <CiMail className="h-8 w-8" />
               </a>
               <p className="hover:cursor-pointer">kdoz@live.com</p>
             </div>
@@ -37,12 +37,12 @@ export default function Contact() {
         </div>
 
         {/* Social Media Section */}
-        <div className="bg-white relative rounded-xl shadow-xl p-8 flex flex-col items-start justify-between w-full sm:w-1/2 sm:h-68 space-y-6 transition-transform transform hover:scale-105 hover:shadow-2x overflow-hidden">
-        <div className="absolute w-180 h-180 bg-blue-600 rounded-full opacity-50 top-[-3rem] left-20 z-10"></div>
-          <h1 className="text-3xl font-semibold text-gray-800 mb-4 z-20">SOCIALS</h1>
+        <div className="con relative rounded-xl shadow-xl p-8 flex flex-col items-start justify-between w-full sm:w-1/2 sm:h-68 space-y-6 transition-transform transform hover:scale-105 hover:shadow-2x overflow-hidden">
+        <div className="absolute w-180 h-180 footer rounded-full opacity-50 top-[-3rem] left-20 z-10"></div>
+          <h1 className="text-3xl font-semibold mb-4 z-20">SOCIALS</h1>
           <div className="space-y-4 z-20">
             {/* Facebook */}
-            <div className="flex items-center text-lg text-gray-700 space-x-4 transition-all hover:text-[#f4ca4f]">
+            <div className="flex items-center text-lg space-x-4 transition-all hover:text-[#f4ca4f]">
               <a href="https://web.facebook.com/nxs88">
                 <BsFacebook className="w-8 h-8 text-blue-500" />
               </a>
@@ -50,7 +50,7 @@ export default function Contact() {
             </div>
 
             {/* TikTok */}
-            <div className="flex items-center text-lg text-gray-700 space-x-4 transition-all hover:text-[#f4ca4f]">
+            <div className="flex items-center text-lg space-x-4 transition-all hover:text-[#f4ca4f]">
               <a href="https://www.tiktok.com/@nexuscloud_">
                 <FaTiktok className="w-8 h-8 text-black" />
               </a>
@@ -58,7 +58,7 @@ export default function Contact() {
             </div>
 
             {/* YouTube */}
-            <div className="flex items-center text-lg text-gray-700 space-x-4 transition-all hover:text-[#f4ca4f]">
+            <div className="flex items-center text-lg space-x-4 transition-all hover:text-[#f4ca4f]">
               <a href="https://www.youtube.com/@NexuscloudITSolutions">
                 <FaYoutube className="w-8 h-8 text-red-500" />
               </a>
